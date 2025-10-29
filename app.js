d82496bae8cd91f2ac86c43d60510ef66704787d
evgeniaver95-cell <evgeniaver95@gmail.com>
2025-10-29 15:06:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -81,9 +81,9 @@ function openSectionV2(section) {  // новое имя
         title: section.title,
         message: text,
         buttons: [
-          ...quickButtons,
-          { id: 'cancel', type: 'cancel', text: 'Отмена' }
-        ]
+  ...quickButtons.reverse(), // сначала материалы
+  { id: 'cancel', type: 'cancel', text: 'Отмена' } // потом "Отмена" внизу
+]
       },
       (btnId) => {
         if (btnId == null || btnId === 'cancel') return;
